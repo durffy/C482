69935d24e88b60552c3489a2d025d6b1bc3beb9a
Connor <connor_duffy@hotmail.com>
2019-10-21 07:21:49 -0700

    - Updated the Product class to use Properties (IntegerProperties, etc)   instead of Integers, Strings, etc - Added return values for properties - changed get and set methods to reflect the changes

--- 2019.08.03_C482/InventorySystem_ConnorDuffy/src/Model/Product.java
+++ 2019.08.03_C482/InventorySystem_ConnorDuffy/src/Model/Product.java
@@ -5,6 +5,13 @@
  */
 package Model;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 /**
@@ -12,69 +19,104 @@ import javafx.collections.ObservableList;
  * @author cjd
  */
 public class Product {
-    private ObservableList<Part> associatedParts;
-    private int id,
+    private ObservableList<Part> associatedParts = FXCollections.observableArrayList();
+    private IntegerProperty id,
             stock,
             min,
             max;
-    private String name;
-    private double price;
+    private StringProperty name;
+    private DoubleProperty price;
 
     public Product(int id, String name, double price, int stock, int min, int max) {
-        this.id = id;
-        this.stock = stock;
-        this.min = min;
-        this.max = max;
-        this.name = name;
-        this.price = price;
+        this.id = new SimpleIntegerProperty();
+        this.id.set(id);
+        
+        this.name = new SimpleStringProperty();
+        this.name.set(name);
+        
+        this.stock = new SimpleIntegerProperty();
+        this.stock.set(stock);
+        
+        this.min = new SimpleIntegerProperty();
+        this.min.set(min);
+        
+        this.max = new SimpleIntegerProperty();
+        this.max.set(max);
+        
+        this.price = new SimpleDoubleProperty();
+        this.price.set(price);
     }
 
-    public int getId() {
+    public IntegerProperty getIdProperty(){
         return id;
+    }
+    
+    public StringProperty getNameProperty(){
+       return name;
+    }
+    
+    public IntegerProperty getStockProperty(){
+        return stock;
+    }
+    
+    public IntegerProperty getMinProperty(){
+        return min;
+    }
+    
+    public IntegerProperty getMaxProperty(){
+        return max;
+    }
+    
+    public DoubleProperty getPriceProperty(){
+        return price;
+    }
+    
+    public int getId() {
+        return id.get();
     }
 
     public void setId(int id) {
-        this.id = id;
+        this.id.set(id);
     }
 
     public int getStock() {
-        return stock;
+        return stock.get();
     }
 
     public void setStock(int stock) {
-        this.stock = stock;
+        this.stock.set(stock);
     }
 
     public int getMin() {
-        return min;
+        return min.get();
     }
 
     public void setMin(int min) {
-        this.min = min;
+        this.min.set(min);
     }
 
     public int getMax() {
-        return max;
+        return max.get();
     }
 
     public void setMax(int max) {
-        this.max = max;
+        this.max.set(max);
     }
 
     public String getName() {
-        return name;
+        return name.get();
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public double getPrice() {
-        return price;
+        return price.get();
     }
 
     public void setPrice(double price) {
-        this.price = price;
+        this.price.set(price);
     }
     
 
@@ -82,8 +124,8 @@ public class Product {
         return associatedParts;
     }
 
-    public void addAssociatedParts(Part part) {
-        this.associatedParts.add(part);
+    public void addAssociatedParts(ObservableList<Part> parts) {
+        this.associatedParts= parts;
     }
     
     public void deleteAssociatedParts(Part associatedPart) {
